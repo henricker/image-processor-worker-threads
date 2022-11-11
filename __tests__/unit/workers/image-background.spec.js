8a6b5739c2a8854d56ab5382e9226a4680db2ab5
henricker <henriquevieira@alu.ufc.br>
2022-11-11 14:53:26 -0300

    tests: add todo tests on image background worker

---
+++ __tests__/unit/workers/image-background.spec.js
@@ -0,0 +1,7 @@
+import { describe, it, expect } from '@jest/globals'
+
+describe('#imageBackground - WORKER', () => {
+    it.todo('Should send business error message if data is invalid')
+    it.todo('Should send internal server error message if error is not a business error')
+    it.todo('Should return a base64 image')
+})
