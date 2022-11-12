de1957c13d9cdeca3f1d4464f61349c764328420
henricker <henriquevieira@alu.ufc.br>
2022-11-12 09:24:15 -0300

    test: send internal server error message to user if any server error appear'

--- __tests__/unit/workers/image-background.spec.js
+++ __tests__/unit/workers/image-background.spec.js
@@ -1,11 +1,13 @@
-import { describe, it, expect, beforeEach, afterEach, jest } from '@jest/globals'
-import { ImageBackgroundWorker } from '../../../src/workers/image-background.worker.js'
+import { describe, it, expect, beforeEach, afterEach, jest, beforeAll } from '@jest/globals'
 import { parentPortMock } from './mocks/parent-port-mock.js'
+import axios from 'axios'
+
+import { ImageBackgroundWorker } from '../../../src/workers/image-background.worker.js'
 
 describe('#imageBackground - WORKER', () => {
     let imageBackgroundWorker = new ImageBackgroundWorker({ parentPort: parentPortMock })
 
-    beforeEach(() => {
+    beforeEach(async () => {
         imageBackgroundWorker = new ImageBackgroundWorker({ parentPort: parentPortMock })
     })
     afterEach(() => {
@@ -21,13 +23,26 @@ describe('#imageBackground - WORKER', () => {
             backgroundUrl: 'invalid-url'
         }
 
-       imageBackgroundWorker.handleMessage(message)
-       .catch(_ => {
-            expect(parentPortMock.emit).toHaveBeenCalledWith('messageError', 'imageUrl must be a valid URL, backgroundUrl must be a valid URL')
-            expect(parentPortMock.removeAllListeners).toBeCalled()
-            expect(parentPortMock.unref).toBeCalled()
-       })
+       await imageBackgroundWorker.handleMessage(message)
+     
+			expect(parentPortMock.emit).toHaveBeenCalledWith('messageerror', 'imageUrl must be a valid URL, backgroundUrl must be a valid URL')
+			expect(parentPortMock.removeAllListeners).toBeCalled()
+			expect(parentPortMock.unref).toBeCalled()
+    
     })
-    it.todo('Should send internal server error message if error is not a business error')
+    it('Should send internal server error message if error is not a business error', async () => {
+        const message = {
+            imageUrl: 'https://google.com',
+            backgroundUrl: 'https://google.com'
+        }
+
+				jest.spyOn(axios, 'get').mockRejectedValueOnce(new Error('any_error'))
+  
+        await imageBackgroundWorker.handleMessage(message)
+
+				expect(parentPortMock.emit).toHaveBeenCalledWith('messageerror', 'Internal server error')
+       	expect(parentPortMock.removeAllListeners).toBeCalled()
+				expect(parentPortMock.unref).toBeCalled()
+    })
     it.todo('Should return a base64 image')
 })
