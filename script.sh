747654fc068af42485341c3b5c520603c6b64e03
henricker <henriquevieira@alu.ufc.br>
2022-11-14 11:24:31 -0300

    feat: add script to testing with autocannon

---
+++ script.sh
@@ -0,0 +1,3 @@
+curl -X POST -d \
+"{\"imageUrl\":\"https://static.wikia.nocookie.net/mkwikia/images/e/ee/Predator_render.png\", \"backgroundUrl\": \"https://wallpaperaccess.com/full/3057585.jpg\"}" \
+http://localhost:3000 
